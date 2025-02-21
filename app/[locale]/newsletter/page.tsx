d8c3eaa4bd3067a3b22b5f44b310de5c25fd4f14
Kevin Hsu <wigohunter@kevindeair.lan>
2025-02-20 22:42:07 -0800

    v1 of fantasy page

--- app/[locale]/newsletter/page.tsx
+++ app/[locale]/newsletter/page.tsx
@@ -64,7 +64,7 @@ export default function PageNewsLetter() {
         },
     ];
 
-    return <div className="bg-white py-12 sm:py-12">
+    return <div className="bg-white py-12 sm:py-16">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-xl">
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{t('title')}</h2>
