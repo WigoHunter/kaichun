7ff494930885bf45d46631544af2be5a9697650a
Kevin Hsu <wigohunter@kevindeair.lan>
2025-02-23 22:16:46 -0800

    add some padding for menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -146,7 +146,7 @@ export default function Navbar() {
                     </div>
                     <div className="mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/10">
-                            <div className="space-y-2 py-6">
+                            <div className="space-y-2 py-6 pt-12">
                                 <a
                                     href={`/${locale}`}
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
