4c2efc281ace0eb13a08ba295dd0a8bfe9c9d73f
Kevin Hsu <wigohunter@kevindeair.lan>
2025-02-27 00:05:45 -0800

    smaller font

--- app/[locale]/fantasy/page.tsx
+++ app/[locale]/fantasy/page.tsx
@@ -16,79 +16,79 @@ export default function PageFantasy() {
                         className="rounded-xl bg-gray-50 object-contain"
                     />
                 </figure>
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}烈日之下，好幾滴汗水從萊凱的下巴滑落。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}烈日之下，好幾滴汗水從萊凱的下巴滑落。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}他看著汗水沒入腳下的肥沃黑土，變得不見，成為土壤的養分。然而，汗水與鹽分也還遠遠不是這塊土地從他身上奪取最多的資源。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}他看著汗水沒入腳下的肥沃黑土，變得不見，成為土壤的養分。然而，汗水與鹽分也還遠遠不是這塊土地從他身上奪取最多的資源。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}時間才是。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}時間才是。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}他打直了腰，立刻感受到一股深入骨髓裡的酸痛感。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}他打直了腰，立刻感受到一股深入骨髓裡的酸痛感。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}他已經彎著腰採收魔豆太久了。三個小時了嗎⋯⋯不，才三小時嗎？</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}他已經彎著腰採收魔豆太久了。三個小時了嗎⋯⋯不，才三小時嗎？</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「喂！太陽都曬到頭頂了，別傻站著了。都停下來歇口氣！」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「喂！太陽都曬到頭頂了，別傻站著了。都停下來歇口氣！」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}遠處響起領工的吆喝聲，像是一把利刀劃破了魔豆田裡的寂靜。萊凱鬆了一口氣，放下手中帶有生鏽鋼板的鉗子，拖著沈重的步伐走向田埂。他注意到自己手掌上已經被粗糙的魔豆莢磨出了一層薄薄的血痕，但這在瑟雷斯村裡並不算什麼。他看向四周，附近其他農工或多或少都有類似的傷痕，有些甚至更嚴重。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}遠處響起領工的吆喝聲，像是一把利刀劃破了魔豆田裡的寂靜。萊凱鬆了一口氣，放下手中帶有生鏽鋼板的鉗子，拖著沈重的步伐走向田埂。他注意到自己手掌上已經被粗糙的魔豆莢磨出了一層薄薄的血痕，但這在瑟雷斯村裡並不算什麼。他看向四周，附近其他農工或多或少都有類似的傷痕，有些甚至更嚴重。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}他坐在田埂上，從腰間掏出一個布包，裡面包著僅僅幾片乾柴的吐司。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}他坐在田埂上，從腰間掏出一個布包，裡面包著僅僅幾片乾柴的吐司。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}他熟悉地忽略開始微微發霉的邊角。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}他熟悉地忽略開始微微發霉的邊角。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}很快地，幾個與萊凱年齡相仿，十五、六歲的農工也聚攏過來。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}很快地，幾個與萊凱年齡相仿，十五、六歲的農工也聚攏過來。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「唉，」其中一個黑髮青年一邊用右臂擦汗，一邊哀怨道，「天啊，這次的收成到底還要多久才會結束啊？」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「唉，」其中一個黑髮青年一邊用右臂擦汗，一邊哀怨道，「天啊，這次的收成到底還要多久才會結束啊？」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「可能還要兩、三週吧⋯⋯」另一人回答，隨即補充，「不過，你瞧瞧今年的收穫，應該能有不錯的待遇吧？」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「可能還要兩、三週吧⋯⋯」另一人回答，隨即補充，「不過，你瞧瞧今年的收穫，應該能有不錯的待遇吧？」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「待遇？」黑髮青年冷哼一聲。「是能多分到幾片麵包，還是少挨幾次鞭子？」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「待遇？」黑髮青年冷哼一聲。「是能多分到幾片麵包，還是少挨幾次鞭子？」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「別這麼說，這兒的貴族老爺還算是有點良心的。」年長一些的農民低聲說道，像是在安撫情緒。「至少不像我以前待過的地方，有些工人連性命都不見得能保得住⋯⋯」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「別這麼說，這兒的貴族老爺還算是有點良心的。」年長一些的農民低聲說道，像是在安撫情緒。「至少不像我以前待過的地方，有些工人連性命都不見得能保得住⋯⋯」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「噗，這話聽起來真可笑。」一個熟悉的聲音從萊凱身旁響起。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「噗，這話聽起來真可笑。」一個熟悉的聲音從萊凱身旁響起。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}萊凱轉頭，看見海頓拎著一個破破舊舊的水壺走來，隨意地坐在他的身旁。這是萊凱在農舍裡的室友，也是他在這裡最接近是個「朋友」的人。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}萊凱轉頭，看見海頓拎著一個破破舊舊的水壺走來，隨意地坐在他的身旁。這是萊凱在農舍裡的室友，也是他在這裡最接近是個「朋友」的人。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「海頓前輩，你怎麼還有力氣嘲笑別人？」萊凱苦笑道。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「海頓前輩，你怎麼還有力氣嘲笑別人？」萊凱苦笑道。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「我只是說實話。」海頓抿了一口水，然後把水壺遞給萊凱。「萊凱啊，你難道覺得光是『能夠保住性命』就夠了嗎？」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「我只是說實話。」海頓抿了一口水，然後把水壺遞給萊凱。「萊凱啊，你難道覺得光是『能夠保住性命』就夠了嗎？」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}萊凱誠實地搖了搖頭。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}萊凱誠實地搖了搖頭。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}一旁的年長農民嘆了一口氣。「那是你們還沒見過更糟的。」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}一旁的年長農民嘆了一口氣。「那是你們還沒見過更糟的。」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「哼，你才是缺乏眼界，沒見過更好的生活。」海頓說。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「哼，你才是缺乏眼界，沒見過更好的生活。」海頓說。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}年長的農工盤起雙手，像是要說些什麼，但是萊凱搶著問道：「但是，海頓前輩，我很好奇⋯⋯你說的『更好的生活』又是怎麼樣？」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}年長的農工盤起雙手，像是要說些什麼，但是萊凱搶著問道：「但是，海頓前輩，我很好奇⋯⋯你說的『更好的生活』又是怎麼樣？」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}年長的農民點了點頭，附和著萊凱的問題。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}年長的農民點了點頭，附和著萊凱的問題。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}附近的年輕農民一邊吃著簡陋的麵包午餐，一邊默默地把視線與注意力集中過來。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}附近的年輕農民一邊吃著簡陋的麵包午餐，一邊默默地把視線與注意力集中過來。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「這個嘛⋯⋯真是一個好問題，萊凱。」海頓似乎很享受眾人的視線。他清了清嗓子，然後說道：「我想去瓦提亞。」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「這個嘛⋯⋯真是一個好問題，萊凱。」海頓似乎很享受眾人的視線。他清了清嗓子，然後說道：「我想去瓦提亞。」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「瓦提亞？」萊凱重複道。「那不就只是另一個種植魔豆的村子？」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「瓦提亞？」萊凱重複道。「那不就只是另一個種植魔豆的村子？」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「不，是另一個『更好的』種植魔豆的村子。」海頓搖了搖食指。「我聽說，比起我們這裡又傳統又古板，而且還累死人的採收方式，瓦提亞那邊在研究更有效率的種植與採收技術。」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「不，是另一個『更好的』種植魔豆的村子。」海頓搖了搖食指。「我聽說，比起我們這裡又傳統又古板，而且還累死人的採收方式，瓦提亞那邊在研究更有效率的種植與採收技術。」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「那不過是因為他們能種植魔豆的地更小。」年長的農民說道。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「那不過是因為他們能種植魔豆的地更小。」年長的農民說道。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「是啊，但又怎麼樣？難道種了更多的魔豆會是你的嗎？你在這裡辛苦採收這麼一大塊地的魔豆，還不是都給貴族大人們拿去了。」海頓毫不客氣地說道。「我還寧可去研究新的技術、新的知識⋯⋯新的挑戰。」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「是啊，但又怎麼樣？難道種了更多的魔豆會是你的嗎？你在這裡辛苦採收這麼一大塊地的魔豆，還不是都給貴族大人們拿去了。」海頓毫不客氣地說道。「我還寧可去研究新的技術、新的知識⋯⋯新的挑戰。」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}萊凱注意到海頓在說這話時，雙眼是在發光的。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}萊凱注意到海頓在說這話時，雙眼是在發光的。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「那很遠欸。」萊凱說道。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「那很遠欸。」萊凱說道。</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「怎麼？」海頓揚起一邊的眉毛。「你會想我嗎？」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「怎麼？」海頓揚起一邊的眉毛。「你會想我嗎？」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「不。」萊凱乾笑了一下。「只是，你哪來的錢去那麼遠的地方？」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「不。」萊凱乾笑了一下。「只是，你哪來的錢去那麼遠的地方？」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「我正在存錢。」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「我正在存錢。」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「是嗎？」萊凱半信半疑地說道：「另外，你肯定會半路就迷路的。」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「是嗎？」萊凱半信半疑地說道：「另外，你肯定會半路就迷路的。」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}「哼，才不會。」海頓說。「我要是連地圖都看不懂，還談什麼研究？我可是很聰明的。」</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}「哼，才不會。」海頓說。「我要是連地圖都看不懂，還談什麼研究？我可是很聰明的。」</p>
 
-                <p className="mt-6 text-xl leading-8">{`\u3000\u3000`}氣氛變得稍微輕鬆，彷彿正午的懊熱都變得沒那麼難耐。</p>
+                <p className="mt-6 text-l leading-8">{`\u3000\u3000`}氣氛變得稍微輕鬆，彷彿正午的懊熱都變得沒那麼難耐。</p>
 
 
             </div>
