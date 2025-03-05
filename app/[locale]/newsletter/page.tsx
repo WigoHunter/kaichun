f38978a69b501f8a9e3dd63b065c5f9a4ba61937
Kevin Hsu <wigohunter@kevindeair.lan>
2025-03-04 22:09:27 -0800

    fix overflow

--- app/[locale]/newsletter/page.tsx
+++ app/[locale]/newsletter/page.tsx
@@ -8,7 +8,7 @@ export default function PageNewsLetter() {
     const locale = useLocale();
     const posts = getPosts(locale);
 
-    return <div className="bg-white py-12 sm:py-16">
+    return <div className="bg-white py-12 sm:py-16 overflow-hidden">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-xl">
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{t('title')}</h2>
