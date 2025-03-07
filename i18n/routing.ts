5b9cf2c26a92bee9ebbe8e5b983b93aba088d617
Kevin Hsu <wigohunter@kevindeair.lan>
2025-03-07 09:38:52 -0800

    default locale to zh

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,12 +1,12 @@
-import {defineRouting} from 'next-intl/routing';
-import {createNavigation} from 'next-intl/navigation';
- 
+import { defineRouting } from 'next-intl/routing';
+import { createNavigation } from 'next-intl/navigation';
+
 export const routing = defineRouting({
   locales: ['en', 'zh'],
-  defaultLocale: 'en'
+  defaultLocale: 'zh'
 });
- 
+
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
-export const {Link, redirect, usePathname, useRouter, getPathname} =
+export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);
